7cc9f4cd4d27f962e07ec66a90f15c9a0af6f8e4
miljan003@o365.student.utp.edu.pl <miljan003@o365.student.utp.edu.pl>
2020-10-04 15:20:28 +0200

    add useEffect

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SingleTodo from "./SingleTodo";
 const TodoList = () => {
   const [todos, setTodos] = useState(["Example Todo1", "Example Todo2"]);
   const [text, setText] = useState("");
+  const [disabled, setDisabled] = useState(false);
 
+  useEffect(() => {
+    if (todos.length >= 5) setDisabled(true);
+    else setDisabled(false);
+  }, [todos]);
   const DeleteFromTodos = (TodoIndex) => {
     //Todos with removed item at index
     const newTodos = todos.filter((el, index) => index !== TodoIndex);
@@ -24,6 +29,7 @@ const TodoList = () => {
         ></SingleTodo>
       ))}
       <input
+        disabled={disabled}
         style={{ width: "100%", boxSizing: "border-box" }}
         type="text"
         placeholder="Enter Todo Task"
